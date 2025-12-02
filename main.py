4941db6d260241d7912cdaa701e6bb71d836e85a
AMIR-SHARIPOV <amir_sharipov_04@bk.ru>
2025-12-02 17:00:33 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from database.connection import conn
-
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import Settings
 
 import uvicorn
-app = FastAPI()
-# Register routes
+from contextlib import asynccontextmanager
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings = Settings()
+    await settings.initialize_database()
+    print("MongoDB connected and Beanie initialized")
+    yield
+    print("Application shutdown")
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
-
-
-@app.on_event("startup")
-def on_startup():
-    conn()
 
 
 @app.get("/")
 async def home():
     return RedirectResponse(url="/event/")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
-
-
--- models/users.py
+++ models/users.py
@@ -1,42 +1,27 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
 from models.events import Event
 
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
-    events: Optional[List[Event]]
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "email": "fastapi@packt.com",
-                "events": [],
-            }
-        }
-
-
-class NewUser(User):
     password: str
+    events: Optional[List[Link[Event]]]
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "email": "fastapi@packt.com",
-                "password": "strong!!!",
-                "events": [],
+    class Settings:
+        name = "users"
+
+        class Config:
+            schema_extra = {
+                "example": {
+                    "email": "fastapi@packt.com",
+                    "password": "strong!!!",
+                    "events": [],
+                }
             }
-        }
 
 
 class UserSignIn(BaseModel):
     email: EmailStr
     password: str
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "email": "fastapi@packt.com",
-                "password": "strong!!!",
-            }
-        }
